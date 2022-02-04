f265f265328815cfacbb6db8ab389a4e5e553859
Admin <baconnkl@gmail.com>
2022-02-04 05:52:12 +0300

    create CharactersAdapter.kt

--- app/src/main/java/com/bacon/graphqlrickandmorty/ui/adapters/CharactersAdapter.kt
+++ app/src/main/java/com/bacon/graphqlrickandmorty/ui/adapters/CharactersAdapter.kt
@@ -7,10 +7,14 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.bacon.graphqlrickandmorty.CharactersListQuery
+import com.bacon.graphqlrickandmorty.common.extension.setOnSingleClickListener
 import com.bacon.graphqlrickandmorty.databinding.ItemCharacterBinding
 
 
-class CharactersAdapter :
+class CharactersAdapter(
+    val onItemLongClick: (photo: String) -> Unit,
+    val onItemClick: (id: String) -> Unit,
+) :
     ListAdapter<CharactersListQuery.Result, CharactersAdapter.CharacterViewHolder>(CharacterDiffUtil()) {
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
         getItem(position)?.let { holder.onBind(it) }
@@ -26,10 +30,24 @@ class CharactersAdapter :
         private val binding: ItemCharacterBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
 
+        init {
+            itemView.setOnSingleClickListener {
+                getItem(adapterPosition)?.let {
+                    onItemClick(it.id.toString())
+                }
+            }
+        }
+
         fun onBind(item: CharactersListQuery.Result) = with(binding) {
             itemName.text = item.name
             itemImage.load(item.image)
             itemStatus.text = item.status
+            itemImage.setOnLongClickListener {
+                getItem(adapterPosition)?.let {
+                    onItemLongClick(it.image.toString())
+                }
+                true
+            }
         }
     }
 
