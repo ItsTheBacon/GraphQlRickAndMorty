6b9799acf06b5e45649ce96a3773a11a5202af17
Admin <baconnkl@gmail.com>
2022-02-04 05:54:06 +0300

    added fetch for detail character

--- app/src/main/java/com/bacon/graphqlrickandmorty/data/repositories/CharactersRepository.kt
+++ app/src/main/java/com/bacon/graphqlrickandmorty/data/repositories/CharactersRepository.kt
@@ -2,6 +2,7 @@ package com.bacon.graphqlrickandmorty.data.repositories
 
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.coroutines.await
+import com.bacon.graphqlrickandmorty.CharacterQuery
 import com.bacon.graphqlrickandmorty.CharactersListQuery
 import com.bacon.graphqlrickandmorty.data.apiservices.CharactersApiService
 import javax.inject.Inject
@@ -11,5 +12,8 @@ class CharactersRepository @Inject constructor(
 ) {
     suspend fun queryCharactersList(): Response<CharactersListQuery.Data> {
         return service.getApolloClient().query(CharactersListQuery()).await()
+    }
+    suspend fun queryCharacter(id: String): Response<CharacterQuery.Data> {
+        return service.getApolloClient().query(CharacterQuery(id)).await()
     }
 }
